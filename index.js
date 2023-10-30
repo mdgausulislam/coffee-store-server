b12d3b89b02983b93699230d2138e8fa39cbeed7
unknown <gausulislam5@gmail.com>
2023-10-30 16:24:38 +0600

    update coffe in connect database

--- index.js
+++ index.js
@@ -52,6 +52,21 @@ async function run() {
             res.send(result)
         })
 
+        app.put('/coffee/:id',async(req,res)=>{
+            const id =req.params.id;
+            const filter={_id:new ObjectId(id)}
+            const options={upsert: true}
+            const updateCoffee=req.body;
+            const coffee={
+                $set:{
+                    name:updateCoffee.name, quantity:updateCoffee.quantity, supplier:updateCoffee.supplier, taste:updateCoffee.taste, category:updateCoffee.category, details:updateCoffee.details,
+                    photo:updateCoffee.photo,
+                }
+            }
+            const result=await coffeeCollection.updateOne(filter,coffee,options);
+            res.send(result)
+        })
+
         app.delete('/coffee/:id',async(req,res)=>{
             const id=req.params.id;
             const query={_id:new ObjectId(id)}
